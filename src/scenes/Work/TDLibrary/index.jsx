64de6a5e10ce01a6358ac25ebe2c19fda9e3aa72
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-08-02 18:42:14 -0400

    update

--- src/scenes/Work/TDLibrary/index.jsx
+++ src/scenes/Work/TDLibrary/index.jsx
@@ -1,8 +1,8 @@
 import React              from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link }           from 'react-router-dom';
-import TDLibraryEN        from './TDLibraryEN.mdx';
 
+import TDLibraryEN from './TDLibraryEN.mdx';
 import TDLibraryFR from './TDLibraryFR.mdx';
 
 const TDLibrary = () => {
@@ -16,9 +16,9 @@ const TDLibrary = () => {
           &#10232;<span>{ t('work.back') }</span>
         </Link>
       </h1>
-      { i18n.language ? <TDLibraryFR /> : <TDLibraryEN /> }
+      { i18n.language === 'fr' ? <TDLibraryFR/> : <TDLibraryEN/> }
     </main>
-  )
-}
+  );
+};
 
 export default TDLibrary;
